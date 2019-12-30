0d1414ad88c19d7457853142d7fdb0063fa45f40
Ky Huynh <ky.huynh@antbuddyc.com>
2019-12-30 18:43:55 +0630

    Now SETUPDIR variable available

--- bash/common.sh
+++ bash/common.sh
@@ -212,7 +212,7 @@ verify_phone() {
 fn_exists() { test x$(type -t $1) = xfunction; }
 
 oliver-common-exec() {
-  SETUPDIR=$(dirname $0)
+  SETUPDIR=$( dirname $(realpath "$0") )
   if [[ "$1" == "--check-existed" ]]; then
     arg=$2
     shift
