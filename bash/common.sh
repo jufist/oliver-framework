a2eeed0b9dab9dba5b280886bc53980e9782bcf2
Ky Huynh <ky.huynh@antbuddyc.com>
2020-02-26 16:18:42 +0630

    Oliver Stack

--- bash/common.sh
+++ bash/common.sh
@@ -275,3 +275,6 @@ execInEachType () {
 		fi
 	done
 }
+
+OLIVERDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd ../ >/dev/null && pwd )"
+# PATH="$PATH:$OLIVERDIR/scripts"
--- examples/test.sh
+++ examples/test.sh
@@ -1,17 +1,3 @@
-# Oliver Framework
-## Setup
-- package.json
-```
-  "dependencies": {
-    "oliver-framework": "git@github.com:antbuddy-share/oliver-framework.git"
-  }
-```
-
-- `npm install`
-
-## Using bash structure exec
-
-```bash
 #!/bin/bash
 
 exec--test() {
@@ -21,6 +7,7 @@ exec--test() {
 exec--main() {
   local cmd=`basename $0`
   declare -F | grep exec-- | sed 's/declare -f exec/'$cmd' /'
+  echo $OLIVERDIR
 }
 
 vars_parse--add() {
@@ -40,15 +27,5 @@ exec--add() {
   echo "$@"
 }
 
-. node_modules/oliver-framework/bash/common.sh
-
+. ../bash/common.sh
 oliver-common-exec --check-existed '$M0 $M1' "$@"
-
-```
-
-# Using Docker Oliver Stack
-`. node_modules/oliver-framework/bash/oliverstack.sh`
-`oliver-stack --help`
-
-# Variables
-OLIVERDIR
