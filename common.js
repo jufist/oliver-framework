976412376c672ed954f6480e2429639583edd533
Oliver Huynh <oliver@jufist.com>
2021-09-05 00:34:57 +0700

    Quick

--- common.js
+++ common.js
@@ -452,7 +452,8 @@ GM.exportbash = function (v) {
       console.log(`${i}=(`);
       Object.keys(v[i]).forEach((ii) => {
         if (typeof v[i][ii] != 'object') {
-          const s=JSON.stringify(`${v[i][ii]}`);
+          // Allow \n to render normally in bash
+          const s=JSON.stringify(`${v[i][ii]}`).replaceAll("\\n", "\n");
           console.log(`  [${ii}]=${s}`);
         }
       });
