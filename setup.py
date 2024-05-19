9b41aca162227382a9e6e33f3e0ae204f53aa455
Oliver Huynh <oliver@jufist.com>
2024-05-20 00:45:24 +0700

    AnotherTest

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='oliver_framework',
+    version='0.1.10',
+    packages=find_packages(),
+    description='Oliver Python Framework',
+    author='Oliver Huynh',
+    author_email='oliver@jufist.com',
+    url='https://github.com/oliverhuynh/oliver-framework',
+    package_dir={'oliver_framework': 'python'},  
+    install_requires=[
+        'azure-storage-blob',
+        'pyodbc',
+        'python-dotenv',
+        'schedule',
+        'pydispatcher',
+        'sqlalchemy',
+        'colorlog',
+        'pystray',
+        'Pillow',  # This is the PIL fork for handling images
+        'tk',  # tk is usually included with Python, but this might not install it as expected
+        'sqlparse',
+    ],
+)
