8b44d4af30e4c86d9ccb52558b642f71987441d1
Oliver Huynh <oliver@jufist.com>
2024-10-05 10:47:29 +0700

    B

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='oliver_framework',
-    version='0.1.15',
+    version='0.1.16',
     packages=find_packages(),# ['oliver_framework.utils'],
     description='Oliver Python Framework',
     author='Oliver Huynh',
